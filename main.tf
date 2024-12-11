2859a8c7ae8947b3415d4c01403eb1e0ea398e42
ashbel <ashbelraj@gmail.com>
2024-12-11 11:22:44 +0530

    Adding terraform files

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = {
+    Environment = "Production"
+    Project     = "StaticWebsite"
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "s3:GetObject"
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+        Principal = "*"
+      }
+    ]
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+#   default = "us-east-1"
+  description = "The AWS region"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+}
