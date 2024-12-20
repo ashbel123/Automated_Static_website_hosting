3ef807459f07d3657c0ddae97809ff27ec03e2d4
ashbel <ashbelraj@gmail.com>
2024-12-20 13:52:01 +0530

    modifying code

--- main.tf
+++ main.tf
@@ -27,5 +27,22 @@ resource "aws_s3_bucket_object" "static_files" {
   )
 }
 
+# Bucket Policy to Allow Public Access
+resource "aws_s3_bucket_policy" "static_website_policy" {
+  bucket = aws_s3_bucket_website_configuration.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket_website_configuration.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
 
 
