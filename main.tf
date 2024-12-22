17508b54362bbc9f17624e9394be99b7b0bc9ea3
ashbel <ashbelraj@gmail.com>
2024-12-22 20:18:40 +0530

    changed code

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
-resource "aws_s3_bucket_website_configuration" "static_website" {
+resource "aws_s3_bucket" "static_website" {
   bucket = var.bucket_name  # Replace with your bucket name
   # acl    = "public-read"
+
+  # index_document {
+  #   suffix = "index.html"
+  # }
+
+  # error_document {
+  #   key = "error.html"
+  # }
+}
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.static_website.id
 
   index_document {
     suffix = "index.html"
@@ -9,27 +20,27 @@ resource "aws_s3_bucket_website_configuration" "static_website" {
   error_document {
     key = "error.html"
   }
-}
 
-resource "aws_s3_bucket_object" "static_files" {
+}
+resource "aws_s3_object" "static_files" {
   for_each = fileset("${path.module}/zomato_clone", "**")
-  bucket   = aws_s3_bucket_website_configuration.static_website.id
+  bucket   = aws_s3_bucket.static_website.id
   key      = each.key
   source   = "${path.module}/zomato_clone/${each.key}"
   content_type = lookup(
     {
       ".html" = "text/html"
       ".css"  = "text/css"
-      ".js"   = "application/javascript"
-      ".png"  = "image/png"
-      ".jpg"  = "image/jpeg"
+      # ".js"   = "application/javascript"
+      # ".png"  = "image/png"
+      # ".jpg"  = "image/jpeg"
     }, regex("\\.[^.]+$", each.key), "application/octet-stream"
   )
 }
 
 # Bucket Policy to Allow Public Access
 resource "aws_s3_bucket_policy" "static_website_policy" {
-  bucket = aws_s3_bucket_website_configuration.static_website.id
+  bucket = aws_s3_bucket.static_website.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -38,7 +49,7 @@ resource "aws_s3_bucket_policy" "static_website_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket_website_configuration.static_website.arn}/*"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
       }
     ]
   })
